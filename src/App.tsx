2351f59b433c419b21ba8a4bd1cbe8d26f9d1813
athsara <athsarafernando470@gmail.com>
2024-07-25 22:35:35 +0530

    Sharing state between components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import Like from "./Components/like";
+import { useState } from "react";
+import NavBar from "./Components/NavBar";
+import Cart from "./Components/Cart";
 
 function App() {
+  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
+
   return (
     <div>
-      <Like onClick={() => console.log("Clicked")} />
+      <NavBar cartItemsCount={cartItems.length} />
+      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
     </div>
   );
 }
